41052e4dbe5748967b76bc7c197ed31886ac9198
Mathious6 <mathious61@gmail.com>
2025-04-10 03:44:33 +0000

    feat: implement BuildHAR function to create HAR format from HTTP request and response

---
+++ converter/har.go
@@ -0,0 +1,40 @@
+package converter
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Mathious6/harkit/harfile"
+)
+
+func BuildHAR(req *http.Request, resp *http.Response) (*harfile.HAR, error) {
+	harReq, err := FromHTTPRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	harResp, err := FromHTTPResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &harfile.HAR{
+		Log: &harfile.Log{
+			Version: "1.2",
+			Creator: &harfile.Creator{
+				Name:    "harkit",
+				Version: "0.2.0",
+			},
+			Entries: []*harfile.Entry{
+				{
+					StartedDateTime: time.Now(),
+					Time:            0,
+					Request:         harReq,
+					Response:        harResp,
+					Cache:           &harfile.Cache{},
+					Timings:         &harfile.Timings{},
+				},
+			},
+		},
+	}, nil
+}
